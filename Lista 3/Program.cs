067c17aa9dcf01c5604bd5bf7867114e196cb0ea
Pedro Camilo <129806995+Dartfei@users.noreply.github.com>
2024-04-29 21:26:20 -0300

    Add files via upload

---
+++ Lista 3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Atividade 1
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite uma sequência de valores inteiros separados por espaço:");
+        string[] input = Console.ReadLine().Split(' ');
+
+        int positivos = 0;
+        int negativos = 0;
+        int zeros = 0;
+
+        foreach (string item in input)
+        {
+            int valor = int.Parse(item);
+
+            if (valor > 0)
+            {
+                positivos++;
+            }
+            else if (valor < 0)
+            {
+                negativos++;
+            }
+            else
+            {
+                zeros++;
+            }
+        }
+
+        Console.WriteLine($"Número de valores positivos: {positivos}");
+        Console.WriteLine($"Número de valores negativos: {negativos}");
+        Console.WriteLine($"Número de zeros: {zeros}");
+    }
+}
+
